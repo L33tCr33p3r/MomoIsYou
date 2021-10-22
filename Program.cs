177365644a1f51d17c01a8d602e3945d7f730bc2
L33t_Cr33p3r <coryvin@thebandit.org>
2021-10-22 14:38:34 -0600

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace MomoIsYou
+{
+	class Program
+	{
+		static void Main()
+		{
+			int[,] GameSpace = new int[5,5] {
+				{00, 00, 00, 00, 00}, 
+				{00, 00, 00, 00, 00}, 
+				{00, 01, 01, 02, 00}, 
+				{00, 00, 00, 00, 00}, 
+				{00, 00, 00, 00, 00}
+			};
+			Console.WriteLine(GameSpace[2, 2]);
+		}
+	}
+}
