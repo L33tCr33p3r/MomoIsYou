5398bfb189c9172e8bfcc1636e0588ae7884e831
L33tCr33p3r <coryvin@thebandit.org>
2021-11-01 22:59:27 -0600

    Added code to open and close SFML Window

--- Program.cs
+++ Program.cs
@@ -3,6 +3,10 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using SFML.System;
+using SFML.Window;
+using SFML.Graphics;
+using SFML.Audio;
 
 
 namespace MomoIsYou
@@ -11,14 +15,30 @@ namespace MomoIsYou
 	{
 		static void Main()
 		{
-			int[,] GameSpace = new int[5,5] {
-				{00, 00, 00, 00, 00}, 
-				{00, 00, 00, 00, 00}, 
-				{00, 01, 01, 02, 00}, 
-				{00, 00, 00, 00, 00}, 
+			//Open SFML Window
+			RenderWindow Window = new RenderWindow(new VideoMode(500, 500), "Momo is You");
+			Window.Closed += new EventHandler(OnClose);
+
+			//Set up a (basic) playing area in an array
+			int[,] GameSpace = new int[5, 5] {
+				{00, 00, 00, 00, 00},
+				{00, 00, 00, 00, 00},
+				{00, 01, 02, 02, 00},
+				{00, 00, 00, 00, 00},
 				{00, 00, 00, 00, 00}
 			};
-			Console.WriteLine(GameSpace[2, 2]);
+
+			//Game Loop
+			while(Window.IsOpen)
+			{
+
+            }
+		}
+		static void OnClose(object sender, EventArgs e)
+		{
+			// Close the window when OnClose event is received
+			RenderWindow window = (RenderWindow)sender;
+			window.Close();
 		}
 	}
 }
