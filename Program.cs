d86a20d53f74b53fee89890280c2a78f3c91ee09
L33tCr33p3r <coryvin@thebandit.org>
2021-11-30 22:28:40 -0700

    (started) adding controls and movement

--- Program.cs
+++ Program.cs
@@ -1,5 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
+using System.Collections;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,18 +10,45 @@ using SFML.Audio;
 
 namespace MomoIsYou
 {
-    class Program
+	class Program
 	{
 		static void Main()
 		{
+			////////////////////////////////////// Set up game variables ////////////////////////////////////
+
+			Queue moveEvents = new Queue();
+			const int UP = 0;
+			const int DOWN = 1;
+			const int LEFT = 2;
+			const int RIGHT = 3;
+
 			//////////////////////////////////////// Open SFML Window ///////////////////////////////////////
 
 			RenderWindow Window = new RenderWindow(new VideoMode(800, 800), "Momo is You");
 			Window.Closed += new EventHandler(OnClose);
 
-			/////////////////////////////// Set up a playing area in an array ///////////////////////////////
+			//////////////////////////////////// Instantiate tile objects ///////////////////////////////////
 
-			int[,] Map = new int[8, 8] {
+			ArrayList tileTypes = new ArrayList();
+
+			Tile air = new Tile();
+			air.Identifier = 0;
+			air.tileColor = Color.White;
+			tileTypes.Add(air);
+
+			Tile momo = new Tile();
+			momo.Identifier = 1;
+			momo.tileColor = Color.Green;
+			tileTypes.Add(momo);
+
+			Tile crate = new Tile();
+			crate.Identifier = 2;
+			crate.tileColor = Color.Red;
+			tileTypes.Add(crate);
+
+			///////////////////////////////////////// Set up the map ////////////////////////////////////////
+
+			int[,] initMap = new int[8, 8] {
 				{00, 00, 00, 00, 00, 00, 00, 00},
 				{00, 00, 02, 00, 00, 00, 00, 00},
 				{00, 00, 00, 00, 00, 00, 00, 00},
@@ -32,57 +59,85 @@ namespace MomoIsYou
 				{00, 00, 00, 00, 00, 00, 00, 00}
 			};
 
-			//////////////////////////////////// Instantiate tile objects ///////////////////////////////////
+			Tile[,] Map = new Tile[initMap.GetLength(0), initMap.GetLength(1)];
 
-			Tile momo = new Tile();
-			momo.mapIdentifier = 1;
-			momo.tileColor = Color.Green;
-
-			Tile crate = new Tile();
-			crate.mapIdentifier = 2;
-			crate.tileColor = Color.Red;
+			for (int i = 0; i < initMap.GetLength(0); i++)
+			{
+				for (int j = 0; j < initMap.GetLength(1); j++)
+				{
+					foreach (Tile tile in tileTypes)
+					{
+						if (tile.Identifier == initMap[i, j])
+						{
+							Map[i, j] = tile;
+						}
+					}
+				}
+			}
 
 			/////////////////////////////////////////// Game Loop ///////////////////////////////////////////
 
 			while (Window.IsOpen)
 			{
-				// Run Event Handlers
+				// Run SFML event handlers
 				Window.DispatchEvents();
 
-				// Game Logic
+				// User input
+				for (int i = 0; i < Map.GetLength(0); i++)
+				{
+					for (int j = 0; j < Map.GetLength(1); j++)
+					{
+						if (Map[i, j].isYou)
+						{
+							if (Keyboard.IsKeyPressed(Keyboard.Key.Up) == true)
+							{
+								moveEvents.Enqueue((Map, Map[i, j], UP, (i, j)));
+							}
+							if (Keyboard.IsKeyPressed(Keyboard.Key.Down) == true)
+							{
+								moveEvents.Enqueue((Map, Map[i, j], DOWN, (i, j)));
+							}
+							if (Keyboard.IsKeyPressed(Keyboard.Key.Left) == true)
+							{
+								moveEvents.Enqueue((Map, Map[i, j], LEFT, (i, j)));
+							}
+							if (Keyboard.IsKeyPressed(Keyboard.Key.Right) == true)
+							{
+								moveEvents.Enqueue((Map, Map[i, j], RIGHT, (i, j)));
+							}
+						}
+					}
+				}
 
-				for (int i = 0; i < 8; i++)
+				// Game logic
+				foreach (Tuple eventArgs in moveEvents)
                 {
-					for (int j = 0; j < 8; j++)
-                    {
-						if (Map[i, j] == 1)
-                        {
-							
-                        }
-                    }
+					Move(moveEvents.Dequeue());
                 }
 
 				// Render section
 				RectangleShape bg = new RectangleShape(new Vector2f(800, 800)) { FillColor = Color.White };
 				Window.Draw(bg);
 
-				for (int i = 0; i < 8; i++)
+				for (int i = 0; i < Map.GetLength(0); i++)
 				{
-					for (int j = 0; j < 8; j++)
+					for (int j = 0; j < Map.GetLength(1); j++)
 					{
-						if (Map[i, j] == 1)
-						{
-							momo.RenderSolid(Window, i, j);
-						}
-						if (Map[i, j] == 2)
-						{
-							crate.RenderSolid(Window, i, j);
-						}
+						Map[i, j].Render(Window, j, i);
 					}
 				}
 				Window.Display();
 			}
 		}
+		static void Move(Tuple moveArgs)
+        {
+			Tile[,] Map;
+			Tile MoveTileType;
+			int moveDirection;
+			int xPos;
+			int yPos;
+			(Map, MoveTileType, moveDirection, (xPos, yPos)) = moveArgs;
+        }
 		static void OnClose(object sender, EventArgs e)
 		{
 			// Close the window when OnClose event is received
--- Tile.cs
+++ Tile.cs
@@ -6,20 +6,28 @@ namespace MomoIsYou
 {
     class Tile
     {
-		public int mapIdentifier;
+		public int Identifier;
 		public Color tileColor;
 		public Texture tileTexture;
+
+		public bool isYou = false;
+		public bool isColide = true;
 		public bool isPush = false;
 		public bool isChainPush = false;
 
-		public void RenderSolid(RenderWindow Window, int i, int j)
+		public void Push(object[,] Map)
+        {
+
+        }
+		public void Render(RenderWindow Window, int x, int y)
         {
 			RectangleShape tileBorder = new RectangleShape(new Vector2f(100, 100));
-			tileBorder.Position = new Vector2f(j * 100, i * 100);
+			tileBorder.Position = new Vector2f(x * 100, y * 100);
 			tileBorder.FillColor = Color.Black;
 			Window.Draw(tileBorder);
+
 			RectangleShape tileCenter = new RectangleShape(new Vector2f(90, 90));
-			tileCenter.Position = new Vector2f(j * 100 + 5, i * 100 + 5);
+			tileCenter.Position = new Vector2f(x * 100 + 5, y * 100 + 5);
 			tileCenter.FillColor = tileColor;
 			Window.Draw(tileCenter);
 		}
